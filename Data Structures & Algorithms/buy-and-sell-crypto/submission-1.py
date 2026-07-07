fba952d7ee9e87702363063951ed30a42357d7b7
Divakar M <diva132006@gmail.com>
2026-07-07 11:15:44 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        day=prices[0]
+        maxP=0
+        for i in range(len(prices)):
+                maxP=max(maxP,prices[i]-day)
+                day=min(day,prices[i])
+        return maxP if maxP>0 else 0
+        
+        
